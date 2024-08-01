1973a54fc52cab5310a44738e1d36fab1d47fa95
Markus Felten <Markus.Felten@gmx.de>
2024-08-01 17:51:31 +0100

    docs: example report

--- src/initialize.mjs
+++ src/initialize.mjs
@@ -4,6 +4,25 @@ import {
 } from "@kronos-integration/service-http";
 import { ServiceCSP } from "./service-csp.mjs";
 
+
+
+/*
+
+{
+  "csp-report": {
+    "blocked-uri": "http://example.com/css/style.css",
+    "disposition": "report",
+    "document-uri": "http://example.com/signup.html",
+    "effective-directive": "style-src-elem",
+    "original-policy": "default-src 'none'; style-src cdn.example.com; report-to /_/csp-reports",
+    "referrer": "",
+    "status-code": 200,
+    "violated-directive": "style-src-elem"
+  }
+}
+
+*/
+
 export default async function initialize(sp) {
   const interceptors = new CTXBodyParamInterceptor();
   interceptors.typeDecoders = Object.assign(
